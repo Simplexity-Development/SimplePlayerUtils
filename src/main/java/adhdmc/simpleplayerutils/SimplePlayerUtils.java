5c92224025913a7593bcb646f782af059c8ee89a
RhythmicSys <mc.adhdmc@gmail.com>
2023-01-12 17:56:07 -0800

    Add PAPI afk stuff

--- src/main/java/adhdmc/simpleplayerutils/SimplePlayerUtils.java
+++ src/main/java/adhdmc/simpleplayerutils/SimplePlayerUtils.java
@@ -4,19 +4,37 @@ import adhdmc.simpleplayerutils.commands.*;
 import adhdmc.simpleplayerutils.commands.inventories.*;
 import adhdmc.simpleplayerutils.listeners.AFKListener;
 import adhdmc.simpleplayerutils.listeners.FlyListeners;
+import adhdmc.simpleplayerutils.util.SPUExpansion;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimplePlayerUtils extends JavaPlugin {
     private static SimplePlayerUtils instance;
     private static final MiniMessage miniMessage = MiniMessage.miniMessage();
+    private boolean purpurLoaded = true;
+    private boolean papiEnabled = true;
 
     @Override
     public void onEnable() {
         instance = this;
         this.getServer().getPluginManager().registerEvents(new FlyListeners(), this);
-        this.getServer().getPluginManager().registerEvents(new AFKListener(), this);
         registerCommands();
+        try {
+            Class.forName("org.purpurmc.purpur.event.PlayerAFKEvent");
+        } catch (ClassNotFoundException e) {
+            this.getLogger().severe("Purpur classes required for purpur-specific functions were not found on your server. The plugin may not perform as intended. Please use updated purpur for intended results.");
+            purpurLoaded = false;
+        }
+        papiEnabled = this.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
+        if (purpurLoaded) {
+            registerPurpurClasses();
+        }
+        if (purpurLoaded && papiEnabled) {
+            new SPUExpansion().register();
+        } else {
+            this.getLogger().severe("Both purpur and placeholder API are needed for Simple Player Utils placeholders, placeholders will be unusable until both of these are present");
+        }
     }
 
     public static SimplePlayerUtils getInstance() {
@@ -25,6 +43,15 @@ public final class SimplePlayerUtils extends JavaPlugin {
 
     public static MiniMessage getMiniMessage() {
         return miniMessage;
+    }
+
+    public boolean isPapiEnabled() {
+        return papiEnabled;
+    }
+
+    private void registerPurpurClasses(){
+        this.getServer().getPluginManager().registerEvents(new AFKListener(), this);
+        this.getCommand("afk").setExecutor(new AFKCommand());
     }
 
     private void registerCommands() {
@@ -41,6 +68,6 @@ public final class SimplePlayerUtils extends JavaPlugin {
         this.getCommand("walkspeed").setExecutor(new WalkspeedCommand());
         this.getCommand("rename").setExecutor(new RenameCommand());
         this.getCommand("hat").setExecutor(new HatCommand());
-        this.getCommand("afk").setExecutor(new AFKCommand());
+
     }
 }
